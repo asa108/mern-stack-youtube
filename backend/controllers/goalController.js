ae4044a5579047b609d883c8e03a01785c685172
Asane <yaradayo317@gmail.com>
2022-02-12 13:48:11 -0500

    installed express-async-handler to avide using then & ctach method

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,20 @@
+const asyncHandler = require('express-async-handler')
+
 // function for goalRoutes
 
 // @desc Get all goals 
 // @route GET /api/goals
 // @access Privete
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
         res.status(200).json({
         message:'Get goals'
     });
-}
+})
 
 // @desc Set goals 
 // @route POST /api/goals
 // @access Privete
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async(req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error ('Please add a text')
@@ -20,25 +22,25 @@ const setGoals = (req, res) => {
        res.status(200).json({
         message:'Create goal'
     });
-}
+})
 
 // @desc Update goals 
 // @route PUT /api/goals/:id
 // @access Privete
-const updateGoals = (req, res) => {
+const updateGoals =asyncHandler( async(req, res) => {
      res.status(200).json({
         message:`Update goal ${req.params.id}`
     });
-}
+})
 
 // @desc Delete all goals 
 // @route DELETE /api/goals/:id
 // @access Privete
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async(req, res) => {
      res.status(200).json({
         message:`Delete goal ${req.params.id}`
     });
-}
+})
 
 module.exports = {
     getGoals,
