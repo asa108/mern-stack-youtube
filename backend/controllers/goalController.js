60a21004ed5d460884fb2a4d02d40ffab4ab52c1
Asane <yaradayo317@gmail.com>
2022-02-12 14:19:52 -0500

    update goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -31,10 +31,14 @@ const setGoals = asyncHandler(async(req, res) => {
 // @desc Update goals 
 // @route PUT /api/goals/:id
 // @access Privete
-const updateGoals =asyncHandler( async(req, res) => {
-     res.status(200).json({
-        message:`Update goal ${req.params.id}`
-    });
+const updateGoals = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{new: true})
+     res.status(200).json(updatedGoal);
 })
 
 // @desc Delete all goals 
